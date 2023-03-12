c3a12fd85880ebda76a8a12987584480e3f59525
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-12 17:18:31 -0500

    add basic routing and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { lazy } from 'react';
+import { Route } from 'react-router';
+import { routes } from './models';
+import { NotFoundRoute } from './utilities';
+
+const Home = lazy(() => import('./pages/home/Home'));
+const ReduxPage = lazy(() => import('./pages/reduxPage/ReduxPage'));
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NotFoundRoute>
+        <Route path={routes.HOME} element={<Home />} />
+        <Route path={routes.REDUX} element={<ReduxPage />} />
+      </NotFoundRoute>
+    </div>
+  );
 }
 
 export default App;
