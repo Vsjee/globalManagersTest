58ff53819850e8f586bd9b5eb8a695e7f015c592
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-12 17:11:22 -0500

    add not found util for the routes

---
+++ src/utilities/NotFoundRoute.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router';
+
+interface IRoutes {
+  children: JSX.Element | JSX.Element;
+}
+
+function NotFoundRoute({ children }: IRoutes) {
+  return (
+    <Routes>
+      {children}
+      <Route path='*' element={<h1>Route not found, try again.</h1>} />
+    </Routes>
+  );
+}
+export default NotFoundRoute;
