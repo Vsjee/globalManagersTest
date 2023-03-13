be8253bb0c50f002fbc044a18e4d9b721b3a12da
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-12 19:11:24 -0500

    fix routing in the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import { router } from './routing';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router}></RouterProvider>
 );
