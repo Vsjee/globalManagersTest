42525b979a0da75aac0ff5ad2d52bcc54a167612
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-14 21:33:39 -0500

    update barrels

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as NavBar } from './NavBar/NavBar';
+export { default as NavBarWrapper } from './NavBar/NavBarWrapper.css';
+export * from './index';
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,6 @@
 export * from './home';
+export { default as Form } from './reduxPage/components/Form/Form';
+export { default as FormWrapper } from './reduxPage/components/Form/FormWrapper.css';
+export { default as TodosList } from './reduxPage/components/TodosList/TodosList';
+export { default as TodosListWrapper } from './reduxPage/components/TodosList/TodosListWrapper.css';
 export * from './reduxPage';
