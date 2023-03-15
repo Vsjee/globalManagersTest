9e6c2b7d317794d42d315fec97bee74ad4350db3
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-15 16:28:33 -0500

    add add todos in zustand store

--- src/pages/zustandPage/components/Form/FormZ.tsx
+++ src/pages/zustandPage/components/Form/FormZ.tsx
@@ -1,12 +1,22 @@
 import { FormEvent, useRef } from 'react';
+import { shallow } from 'zustand/shallow';
+import { myZustandStore, zustandKey } from '../../../../stateManagers';
 import { FormWrapper } from '../../../../styles';
+import { ITodos } from '../../../../types';
+import { addLocalStorage } from '../../../../utilities';
 
 function FormZ() {
+  const [todos, addTodo] = myZustandStore((state) => [state.todos, state.addTodo], shallow);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const currVal = inputRef.current?.value;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const currVal: string | any = inputRef.current?.value;
+    if (currVal !== '') {
+      addTodo(currVal);
+      addLocalStorage<ITodos[]>(zustandKey, todos);
+    }
   };
 
   return (
