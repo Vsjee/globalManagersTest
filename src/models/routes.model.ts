5d784caf7fe46618141363661ffa9fdf908a181e
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-15 12:18:21 -0500

    add and use new route

--- src/models/routes.model.ts
+++ src/models/routes.model.ts
@@ -1,4 +1,5 @@
 export const routes = {
   HOME: '',
   REDUX: 'reduxState',
+  ZUSTAND: 'zustandState',
 };
--- src/routing.tsx
+++ src/routing.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import { routes } from './models';
 import { ReduxPage } from './pages';
+import { ZustandPage } from './pages/zustandPage';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: `/${routes.HOME}`,
     element: <App />,
     children: [
       {
-        path: 'reduxState',
+        path: `${routes.REDUX}`,
         element: <ReduxPage />,
+      },
+      {
+        path: `${routes.ZUSTAND}`,
+        element: <ZustandPage />,
       },
     ],
   },
