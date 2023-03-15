d25b8aab19036fe6a9d3bb5c1d479794902f417c
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-15 16:29:11 -0500

    add get todos from zustand store

--- src/pages/zustandPage/components/TodosList/TodosListZ.tsx
+++ src/pages/zustandPage/components/TodosList/TodosListZ.tsx
@@ -1,17 +1,20 @@
+import { myZustandStore } from '../../../../stateManagers/zustand/zustandStore';
 import { TodosListWrapper } from '../../../../styles';
 
 function TodosListZ() {
+  const [todos] = myZustandStore((state) => [state.todos]);
+
   return (
     <TodosListWrapper>
       <h3 className='sub'>Todos list</h3>
       <ul className='list'>
-        {/* {res.map((todo, i) => {
+        {todos.map((todo, i) => {
           return (
             <li key={i} className='list__item'>
               {todo.task}
             </li>
           );
-        })} */}
+        })}
       </ul>
     </TodosListWrapper>
   );
