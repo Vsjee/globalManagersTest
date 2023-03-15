7d90669c69d6a1e53f3b35147f902da7c8a8bf29
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-15 16:27:23 -0500

    update zustand store

--- src/stateManagers/zustand/zustandStore.ts
+++ src/stateManagers/zustand/zustandStore.ts
@@ -1,15 +1,20 @@
 import { create } from 'zustand';
 import { ITodos } from '../../types';
 
-interface State {
+interface ITodoState {
   todos: ITodos[];
+  addTodo: (textTodo: string) => void;
 }
 
-interface Actions {
-  addTodo: (todo: State['todos']) => void;
-}
+export const zustandKey = 'zustand';
 
-export const myZustandStore = create<State & Actions>((set) => ({
-  todos: [],
-  addTodo: (todo) => set(() => ({ todos: todo })),
+export const myZustandStore = create<ITodoState>((set) => ({
+  todos: localStorage.getItem(zustandKey)
+    ? JSON.parse(localStorage.getItem(zustandKey) as string)
+    : [],
+  addTodo: (textTodo: string) => {
+    set((state) => ({
+      todos: [...state.todos, { id: state.todos.length + 1, task: textTodo, completed: false }],
+    }));
+  },
 }));
