fb71562725a2097b88122e006fafd392e86a90c6
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-16 12:22:08 -0500

    add provider to the jotai page

--- src/pages/jotaiPage/JotaiPage.tsx
+++ src/pages/jotaiPage/JotaiPage.tsx
@@ -1,12 +1,14 @@
+import { Provider } from 'jotai/react';
+import { myJotaiStore } from '../../stateManagers';
 import FormJ from './components/Form/FormJ';
 import TodosListJ from './components/TodosList/TodosListJ';
 
 function JotaiPage() {
   return (
-    <>
+    <Provider store={myJotaiStore}>
       <FormJ />
       <TodosListJ />
-    </>
+    </Provider>
   );
 }
 export default JotaiPage;
