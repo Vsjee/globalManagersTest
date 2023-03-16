7d99797044301b15c0e6f73e42e123d17f7a1e08
david hernandez <davidfelipehernandez1@gmail.com>
2023-03-16 12:21:39 -0500

    use set data and get from the jotai store

--- src/pages/jotaiPage/components/Form/FormJ.tsx
+++ src/pages/jotaiPage/components/Form/FormJ.tsx
@@ -1,11 +1,29 @@
+import { useAtom } from 'jotai/react';
 import { FormEvent, useRef } from 'react';
+import { jotaiKey, myJotaiStore, todos } from '../../../../stateManagers';
 import { FormWrapper } from '../../../../styles';
+import { ITodos } from '../../../../types';
+import { addLocalStorage } from '../../../../utilities';
 
 function FormJ() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [todo] = useAtom(todos);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const currVal: string | any = inputRef.current?.value;
+    if (currVal !== '') {
+      const newTodo = {
+        id: todo.length + 1,
+        task: currVal,
+        completed: false,
+      };
+
+      myJotaiStore.set(todos, [...todo, newTodo]);
+
+      addLocalStorage<ITodos[]>(jotaiKey, [...todo, newTodo]);
+    }
   };
 
   return (
--- src/pages/jotaiPage/components/TodosList/TodosListJ.tsx
+++ src/pages/jotaiPage/components/TodosList/TodosListJ.tsx
@@ -1,17 +1,21 @@
+import { useAtom } from 'jotai/react';
+import { todos } from '../../../../stateManagers';
 import { TodosListWrapper } from '../../../../styles';
 
 function TodosListJ() {
+  const [todo] = useAtom(todos);
+
   return (
     <TodosListWrapper>
       <h3 className='sub'>Todos list</h3>
       <ul className='list'>
-        {/* {todos.map((todo, i) => {
+        {todo.map((todo, i) => {
           return (
             <li key={i} className='list__item'>
               {todo.task}
             </li>
           );
-        })} */}
+        })}
       </ul>
     </TodosListWrapper>
   );
